81fe5335a8ad89bcc3fcc1d91444e5f1ca52fd90
Aditya <adityamaurya0401@gmail.com>
2024-12-18 20:49:07 +0530

    third

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = "http://localhost:5001/posts";  // Fixed the URL
+const url = "https://memorynest.onrender.com/posts";  // Fixed the URL
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
