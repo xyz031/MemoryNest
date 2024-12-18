165d5df272fe5329c1889b17f103451687c8fe9a
Aditya <adityamaurya0401@gmail.com>
2024-12-18 21:28:45 +0530

    fourth

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,18 +1,16 @@
-import { CREATE,DELETE,UPDATE,FETCH_ALL,LIKE } from '../constants/actionTypes.js';
+import { CREATE, DELETE, UPDATE, FETCH_ALL, LIKE } from '../constants/actionTypes.js';
 
-
-
-export default (posts = [],action)=>{  //changes done here directly exports the value by default
+const postsReducer = (posts = [], action) => {  // Define the reducer function first
     switch (action.type) {
         case DELETE:
             console.log('deleted');
-            return posts.filter((post)=> post._id !== action.payload);
+            return posts.filter((post) => post._id !== action.payload);
         case UPDATE:
             console.log('updating card in reducers');
-            return posts.map((post)=> post._id === action.payload._id ? action.payload : post);
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case LIKE:
             console.log('updating card in reducers');
-            return posts.map((post)=> post._id === action.payload._id ? action.payload : post);
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case CREATE:
             console.log('creating card in reducers');
             return [...posts, action.payload];
@@ -21,4 +19,6 @@ export default (posts = [],action)=>{  //changes done here directly exports the 
         default:
             return posts;
     }
-}
+};
+
+export default postsReducer;  // Export the reducer here
